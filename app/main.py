df1c49ff28629904d3c0e25f1f49cf84dbe5f40d
Qbaldo <durowski@gmail.com>
2025-10-18 19:05:34 +0200

    solution #2

--- app/main.py
+++ app/main.py
@@ -7,29 +7,31 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
+    def __init__(self, distance_from_city_center: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         total_price = 0.0
-        for i in cars:
-            if i.clean_mark < self.clean_power:
-                price = self.calculate_washing_price(i)
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                price = self.calculate_washing_price(car)
                 total_price += price
-                self.wash_single_car(i)
+                self.wash_single_car(car)
         return round(total_price, 1)
 
-    def calculate_washing_price(self, cars: list) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
-            cars.comfort_class * (self.clean_power - cars.clean_mark)
-            * (self.average_rating / self.distance_from_city_center), 1)
+            car.comfort_class * (self.clean_power - car.clean_mark) *
+            (self.average_rating / self.distance_from_city_center), 1
+        )
 
-    def wash_single_car(self, cars: list) -> None:
-        cars.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
         new_count = self.count_of_ratings + 1
